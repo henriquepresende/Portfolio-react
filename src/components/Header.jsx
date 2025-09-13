3f7617f9a3d908b57b4307a717fbbcfe193f5eea
Henrique Pereira <ricky1305prr@gmail.com>
2025-09-12 21:22:03 -0300

    fix new header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,31 @@
-import React from "react";
-import "./Header.css";
+import React, { useState } from "react";
+import './Header.css';
 
-export default function Header(){
-    return(
-        <nav className="navbar">
-            <div className="container">
-                <a href="/" className="logo">Henrique</a>
-                <ul className="nav-links">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">Sobre</a></li>
-                    <li><a href="/certificates">Certificados</a></li>
-                    <li><a href="/music">Música</a></li>
-                    <li><a href="/#contact">Contato</a></li>
-                </ul>
-            </div>
-        </nav>
-    );
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <a href="/" className="logo">Henrique</a>
+
+        {/* Botão hambúrguer */}
+        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        <ul className={`nav-links ${isOpen ? 'open' : ''}`}>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">Sobre</a></li>
+          <li><a href="/certificates">Certificados</a></li>
+          <li><a href="/music">Música</a></li>
+          <li><a href="/#contact">Contato</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
