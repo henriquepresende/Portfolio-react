9a99ca7a7fc2c8369a6d2c9e6bd76ddca91f94ac
Henrique Pereira <ricky1305prr@gmail.com>
2025-09-25 19:36:15 -0300

    Feat(Hero): criar hero jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.css";
+
+export default function Hero(){
+    return(
+        <section className="hero">
+            <div className="hero-container">
+                <h1 className="hero-title">Olá, eu sou o Henrique! </h1>
+                <p className="hero-subtitle">Bem-vindo ao meu portfólio em React</p>
+                <a href="#contact" className="hero-btn">Entre em Contato</a>
+            </div>
+        </section>
+    );
+}
